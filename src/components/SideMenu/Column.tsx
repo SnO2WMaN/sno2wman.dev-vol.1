a972f327a4c3e86ebbbe5fa68d01cd041a35fcd4
SnO2WMaN <sno2wman.official@gmail.com>
2020-02-08 22:15:49 +0900

    ✨ メニューバー追加

---
+++ src/components/SideMenu/Column.tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import { IconDefinition } from '@fortawesome/pro-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+type ContainerProps = {
+  icon: IconDefinition
+  href: string
+}
+type Props = { className: string } & ContainerProps
+
+const Component: React.FC<Props> = ({ className, icon, children, href }) => (
+  <li className={className}>
+    <div className="icon-wrap">
+      <FontAwesomeIcon icon={icon} fixedWidth />
+    </div>
+    <span>{children}</span>
+    <a href={href}></a>
+  </li>
+)
+const StyledComponent = styled(Component)`
+  display: flex;
+  align-items: center;
+  padding: 0 24px;
+  height: 64px;
+  user-select: none;
+  position: relative;
+  > .icon-wrap {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 32px;
+    height: 32px;
+    background-color: #2f2f2f;
+    color: white;
+    border-radius: 50%;
+    font-size: 16px;
+    margin-right: 16px;
+  }
+  > span {
+    &::before {
+      content: '';
+      border-left: 1px dotted #2f2f2f;
+      margin-right: 8px;
+    }
+    font-family: 'Montserrat', sans-serif;
+    font-size: 0.8rem;
+    letter-spacing: 0.15em;
+  }
+  > a {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+  }
+`
+
+const Column: React.FC<ContainerProps> = properties => (
+  <StyledComponent {...properties} />
+)
+
+export default Column
--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -1,15 +1,35 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import Column from './Column'
 import License from './License'
+import {
+  faBriefcase,
+  faEnvelopeOpenText,
+  faFileAlt,
+  faIdCard,
+} from '@fortawesome/pro-solid-svg-icons'
+import { useTranslation } from 'react-i18next'
 
 type ContainerProps = {}
 type Props = { className: string } & ContainerProps
 
 const Component: React.FC<Props> = ({ className }) => {
+  const { t } = useTranslation()
   return (
     <div className={className}>
-      <div className="wrap"></div>
+      <ul className="columns">
+        {[
+          { icon: faIdCard, key: 'profile' },
+          { icon: faBriefcase, key: 'works' },
+          { icon: faEnvelopeOpenText, key: 'contact' },
+          { icon: faFileAlt, key: 'blog' },
+        ].map(({ icon, key }) => (
+          <Column icon={icon} key={key} href={`/${key}`}>
+            {t(`sidenav-${key}`)}
+          </Column>
+        ))}
+      </ul>
       <License />
     </div>
   )
@@ -23,11 +43,13 @@ const StyledComponent = styled(Component)`
   overflow: hidden;
   background-color: #fff;
   box-shadow: 0 0 72px 4px rgba(0, 0, 0, 0.125);
-  > .wrap {
+  > .columns {
     flex-grow: 2;
   }
 `
 
-const SideMenu: React.FC<ContainerProps> = () => <StyledComponent />
+const SideMenu: React.FC<ContainerProps> = properties => (
+  <StyledComponent {...properties} />
+)
 
 export default SideMenu
