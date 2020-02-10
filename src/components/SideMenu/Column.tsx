e4e2373e8cc6cbd027c26b0d52ccaa3028e93169
SnO2WMaN <sno2wman.official@gmail.com>
2020-02-10 16:02:24 +0900

    🎨 メニューバーにアニメーションなどを追加

--- src/components/SideMenu/Column.tsx
+++ src/components/SideMenu/Column.tsx
@@ -3,7 +3,9 @@ import styled from 'styled-components'
 
 import { IconDefinition } from '@fortawesome/pro-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import classnames from 'classnames'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 type ContainerProps = {
   icon: IconDefinition
@@ -11,17 +13,24 @@ type ContainerProps = {
 }
 type Props = { className: string } & ContainerProps
 
-const Component: React.FC<Props> = ({ className, icon, children, href }) => (
-  <li className={className}>
-    <div className="icon-wrap">
-      <FontAwesomeIcon icon={icon} fixedWidth />
-    </div>
-    <span>{children}</span>
-    <Link href={href}>
-      <a />
-    </Link>
-  </li>
-)
+const Component: React.FC<Props> = ({ className, icon, children, href }) => {
+  const router = useRouter()
+  const selected = router.pathname === href
+  return (
+    <li className={classnames(className, { selected })}>
+      <div className="icon-wrap">
+        <FontAwesomeIcon icon={icon} fixedWidth />
+      </div>
+      <span>{children}</span>
+      {!selected && (
+        <Link href={href}>
+          <a />
+        </Link>
+      )}
+    </li>
+  )
+}
+
 const StyledComponent = styled(Component)`
   display: flex;
   align-items: center;
@@ -40,6 +49,7 @@ const StyledComponent = styled(Component)`
     border-radius: 50%;
     font-size: 16px;
     margin-right: 16px;
+    box-shadow: 0 8px 24px rgba(0, 0, 0, 0.25);
   }
   > span {
     &::before {
@@ -57,6 +67,32 @@ const StyledComponent = styled(Component)`
     height: 100%;
     top: 0;
     left: 0;
+    z-index: 1;
+  }
+  transition: box-shadow 0.1s, border-left-width 0.25s ease-out;
+  border-left: solid hsl(204, 40%, 69%);
+  border-left-width: 0;
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    box-shadow: 0 0 48px rgba(0, 0, 0, 0.125) inset;
+    opacity: 0;
+    transition: opacity 0.1s ease-out;
+  }
+  &.selected {
+    z-index: 1;
+    border-left-width: 16px;
+    &::after {
+      opacity: 1;
+    }
+  }
+  :not(.selected):hover {
+    box-shadow: 0 0 48px rgba(0, 0, 0, 0.125);
+    z-index: 2;
   }
 `
 
