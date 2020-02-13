dddce21e777fa0e9552a408a14663c4988c51d1f
SnO2WMaN <sno2wman.official@gmail.com>
2020-02-14 05:48:38 +0900

    ♻️ 寄付部分のレイアウトのリファクタリング

---
+++ src/components/Profile/Donate/Column.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import styled from 'styled-components'
+
+type ContainerProps = { icon: JSX.Element; title: string }
+type Props = { className: string } & ContainerProps
+
+const Component: React.FC<Props> = ({ className, icon, title, children }) => (
+  <div className={className}>
+    <div className="title">
+      <div className="icon-wrap">{icon}</div>
+      <h3>{title}</h3>
+    </div>
+    {children}
+  </div>
+)
+
+const StyledComponent = styled(Component)`
+  display: flex;
+  flex-direction: column;
+  padding: 16px 48px;
+  .title {
+    display: flex;
+    align-items: center;
+    margin: 0 auto;
+    margin-bottom: 36px;
+    > .icon-wrap {
+      color: #2f2f2f;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 24px;
+      height: 24px;
+      margin-right: 16px;
+      > svg {
+        width: 100%;
+        height: 100%;
+      }
+    }
+    h3 {
+      color: #2f2f2f;
+      font-size: 0.75rem;
+      font-family: 'Montserrat', sans-serif;
+      font-weight: 500;
+      letter-spacing: 0.25em;
+    }
+  }
+`
+
+const Container: React.FC<ContainerProps> = properties => {
+  return <StyledComponent {...properties} />
+}
+
+export default Container
--- src/components/Profile/Donate/index.tsx
+++ src/components/Profile/Donate/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 
 import IconPepsi from '../../../assets/svg/pepsi.svg'
+import Column from './Column'
 import WidgetButtonsList from './WidgetButtonsList'
 import WishList from './WishList'
 import { faHandsHeart } from '@fortawesome/pro-solid-svg-icons'
@@ -16,39 +17,26 @@ const Component: React.FC<Props> = ({ className }) => (
       <span>DONATE</span> / <span>WISHLIST</span>
     </h2>
     <div className="columns">
-      <div className="column">
-        <div className="column-title">
-          <div className="icon-wrap">
-            <IconPepsi />
-          </div>
-          <h3>BUY ME A PEPSI</h3>
-        </div>
+      <Column title="BUY ME A PEPSI" icon={<IconPepsi />}>
         <WidgetButtonsList />
-      </div>
-      <div className="column">
-        <div className="column-title">
-          <div className="icon-wrap">
-            <FontAwesomeIcon icon={faHandsHeart} />
-          </div>
-          <h3>WISHLIST</h3>
-        </div>
+      </Column>
+      <Column title="WISHLIST" icon={<FontAwesomeIcon icon={faHandsHeart} />}>
         <WishList />
-      </div>
+      </Column>
     </div>
   </section>
 )
 
 const StyledComponent = styled(Component)`
   width: 100%;
-  height: 512px;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   background-color: hsl(225, 12.5%, 92.5%);
+  padding: 48px 0;
   > h2 {
     color: #2f2f2f;
-    margin-top: 32px;
     margin-bottom: 32px;
     font-size: 0.9rem;
     font-family: 'Montserrat', sans-serif;
@@ -59,38 +47,8 @@ const StyledComponent = styled(Component)`
     width: 100%;
     height: 100%;
     display: grid;
-    grid-template-columns: 1fr 2fr;
-  }
-  .column {
-    display: flex;
-    flex-direction: column;
-    padding: 16px 48px;
-  }
-  .column-title {
-    display: flex;
-    align-items: center;
-    margin: 0 auto;
-    margin-bottom: 36px;
-    > .icon-wrap {
-      color: #2f2f2f;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      width: 24px;
-      height: 24px;
-      margin-right: 16px;
-      > svg {
-        width: 100%;
-        height: 100%;
-      }
-    }
-    h3 {
-      color: #2f2f2f;
-      font-size: 0.75rem;
-      font-family: 'Montserrat', sans-serif;
-      font-weight: 500;
-      letter-spacing: 0.25em;
-    }
+    grid-template-columns: auto 1fr;
+    grid-row-gap: 32px;
   }
 `
 
