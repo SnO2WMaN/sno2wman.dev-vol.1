5f456330630b878ebf9ecd4dd59b3c601bb8392d
SnO2WMaN <sno2wman.official@gmail.com>
2020-02-13 22:54:08 +0900

    ♻️ ブランドカラーを別ファイルに纏めた

--- src/components/Profile/Links/index.tsx
+++ src/components/Profile/Links/index.tsx
@@ -7,6 +7,7 @@ import IconKofi from '../../../assets/ko-fi.svg'
 import IconKyash from '../../../assets/kyash.svg'
 import IconPixiv from '../../../assets/pixiv.svg'
 import IconQiita from '../../../assets/qiita.svg'
+import brandcolors from '../../../json/brandcolors.json'
 import SocialLink from './SocialLink'
 import {
   faBitcoin,
@@ -31,112 +32,92 @@ type ContainerProps = {}
 type Props = { className: string } & ContainerProps
 
 const socials: {
-  [key in string]: { href: string; color: string; icon: any; disable?: true }
+  [key in string]: { href: string; icon: any; disable?: true }
 } = {
   'twitter': {
     href: 'https://twitter.com/SnO2WMaN',
     icon: <FontAwesomeIcon icon={faTwitter} />,
-    color: '#1da1f2',
   },
   'github': {
     href: 'https://github.com/SnO2WMaN',
     icon: <FontAwesomeIcon icon={faGithub} />,
-    color: '#24292e',
   },
   'spotify': {
     href: 'https://open.spotify.com/user/sno2wman',
     icon: <FontAwesomeIcon icon={faSpotify} />,
-    color: '#1db954',
   },
   'pixiv': {
     href: 'https://www.pixiv.net/member.php?id=31358190',
     icon: <IconPixiv />,
-    color: '#009cff',
   },
   'soundcloud': {
     href: 'https://soundcloud.com/sno2wman',
     icon: <FontAwesomeIcon icon={faSoundcloud} />,
-    color: '#ff8800',
   },
   'discord': {
     href: 'discord:SnO2WMaN#9459',
     icon: <FontAwesomeIcon icon={faDiscord} />,
-    color: '#7289da',
   },
   'patreon': {
     href: 'https://www.patreon.com/SnO2WMaN',
     icon: <FontAwesomeIcon icon={faPatreon} />,
-    color: '#f86754',
   },
   'annict': {
     href: 'https://annict.jp/@SnO2WMaN',
     icon: <IconAnnict />,
-    color: '#f85b73',
   },
   'ko-fi': {
     href: 'https://ko-fi.com/sno2wman',
     icon: <IconKofi />,
-    color: '#29ABE0',
   },
   'npm': {
     icon: <FontAwesomeIcon icon={faNpm} />,
-    color: '#cb3837',
     href: 'https://www.npmjs.com/~sno2wman',
   },
   'steam': {
     icon: <FontAwesomeIcon icon={faSteamSquare} />,
-    color: '#1b2838',
     href: 'https://steamcommunity.com/id/SnO2WMaN',
   },
   'keybase': {
     icon: <FontAwesomeIcon icon={faKeybase} />,
-    color: '#ff6f21',
     href: 'https://keybase.io/sno2wman',
   },
   'bookmeter': {
     href: 'https://bookmeter.com/users/1011664',
     icon: <IconBookmeter />,
-    color: '#64bc4b',
   },
   'bitcoin': {
     href: 'bitcoin:13EKexdZYnjKaQQAVSrbtwegAN9iAeLBiG',
     icon: <FontAwesomeIcon icon={faBitcoin} />,
-    color: '#f7931a',
     disable: true,
   },
   'qiita': {
     href: 'https://qiita.com/SnO2WMaN',
     icon: <IconQiita />,
-    color: '#55c500',
   },
   'paypal': {
     href: 'https://www.twitch.tv/sno2wman',
     icon: <FontAwesomeIcon icon={faPaypal} />,
-    color: '#003087',
     disable: true,
   },
   'twitch': {
     href: 'https://www.twitch.tv/sno2wman',
     icon: <FontAwesomeIcon icon={faTwitch} />,
-    color: '#6441a5',
     disable: true,
   },
   'youtube': {
     href: 'https://www.youtube.com/channel/UCz_DWmdSbXtBpi1qw3Gg5YQ',
     icon: <FontAwesomeIcon icon={faYoutube} />,
-    color: '#ff0000',
     disable: true,
   },
   'kyash': {
     href: 'kyash://qr/u/3548793821849286552',
     icon: <IconKyash />,
-    color: '#1aaae2',
     disable: true,
   },
   'codepen': {
     href: 'https://codepen.io/SnO2WMaN',
     icon: <FontAwesomeIcon icon={faCodepen} />,
-    color: '#131417',
     disable: true,
   },
 }
@@ -149,14 +130,15 @@ const Component: React.FC<Props> = ({ className }) => (
     <ul className="links">
       {Object.entries(socials)
         .sort(
-          ({ 1: { color: a } }, { 1: { color: b } }) =>
-            ((Color(a).hue() + 30) % 360) - ((Color(b).hue() + 30) % 360)
+          ({ 0: a }, { 0: b }) =>
+            ((Color(brandcolors[a]).hue() + 30) % 360) -
+            ((Color(brandcolors[b]).hue() + 30) % 360)
         )
         .filter(({ 1: { disable } }) => !disable)
-        .map(({ 0: key, 1: { icon, color, href } }, i, a) => (
+        .map(({ 0: key, 1: { icon, href } }, i, a) => (
           <SocialLink
             key={key}
-            color={color}
+            color={brandcolors[key]}
             href={href}
             row={(i % columns) / (columns - 1)}
             line={
